7b5c37e78f9db1d27480ce5be824d2e6e742d823
zhongyuanzhao000 <zhongyuanzhao000@gmail.com>
2019-11-18 03:08:42 -0500

    first

---
+++ src/main/java/demo/jxspring/DemoApplication.java
@@ -0,0 +1,13 @@
+package demo.jxspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
